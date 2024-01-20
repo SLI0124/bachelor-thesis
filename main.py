75124e62dc95366b51d57ad4976d162150c9ada3
Vojtech <sli0124@vsb.cz>
2024-01-20 20:38:21 +0100

    extracting each parking lot from ROI and saving it

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import torch
-
-
-def main():
-    print(torch.__version__)
-
-    if torch.cuda.is_available():
-        print("CUDA is available")
-    else:
-        print("CUDA is not available")
-
+import image_preprocessing as ip
 
 if __name__ == "__main__":
-    main()
+    ip.main(args=None)
