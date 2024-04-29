cce8027ebd94f03de09d92e8249cf1813b2ed0db
Vojtěch Slíva <sli0124@vsb.cz>
2024-04-29 23:56:13 +0200

    init model_summary.py

--- model_visualization/model_summary.py
+++ model_visualization/model_summary.py
@@ -1,2 +1,13 @@
-def sum_up_model(path_to_model):
-    raise NotImplementedError("Implement me!")
+import torchsummary
+import torch
+from torchvision.models import alexnet, squeezenet1_0, shufflenet_v2_x0_5, mobilenet_v2
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = alexnet()
+
+model.to(device)
+
+model.load_state_dict(torch.load("../data/models/cnrpark/cnrpark_ext/80_20/alexnet.pth"))
+
+torchsummary.summary(model, (3, 224, 224))
