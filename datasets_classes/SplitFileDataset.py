c568087ef2c249bb10e6ca530f18426b6ea6d409
Vojtěch Slíva <sli0124@vsb.cz>
2024-05-22 15:40:59 +0200

    new dataset class

--- datasets_classes/SplitFileDataset.py
+++ datasets_classes/SplitFileDataset.py
@@ -4,11 +4,35 @@ from PIL import Image
 
 
 class SplitFileDataset(Dataset):
-    def __init__(self, root_dir, split_file, transform=None):
-        raise NotImplementedError("Implement me!")
+    def __init__(self, file_path, root_dir, transform=None):
+        self.classes = []
+        self.root_dir = root_dir
+        self.transform = transform
+        self.samples = []
+        with open(file_path, 'r') as f:
+            for line in f:
+                path, label = line.split()
+                self.samples.append((path, int(label)))
 
     def __len__(self):
-        raise NotImplementedError("Implement me!")
+        return len(self.samples)
 
     def __getitem__(self, idx):
-        raise NotImplementedError("Implement me!")
+        path, label = self.samples[idx]
+        img = Image.open(os.path.join(self.root_dir, path))
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+    def get_class_distribution(self):
+        class_distribution = {}
+        for _, label in self.samples:
+            if label in class_distribution:
+                class_distribution[label] += 1
+            else:
+                class_distribution[label] = 1
+
+        class_distribution['empty'] = class_distribution.pop(0)
+        class_distribution['occupied'] = class_distribution.pop(1)
+
+        return class_distribution
