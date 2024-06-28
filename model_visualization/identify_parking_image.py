5914344411e7dcea9631abda1bc9b0e0d597cfe4
Vojtěch Slíva <sli0124@vsb.cz>
2024-06-28 16:02:32 +0200

    added pklot identifying

--- model_visualization/identify_parking_image.py
+++ model_visualization/identify_parking_image.py
@@ -1,13 +1,14 @@
 import sys
 import os
+import time
+
 import torch
 import random
 import json
 import cv2
-from PIL import Image
 import csv
 import numpy as np
-from torchvision import transforms
+import xml.etree.ElementTree as ET
 
 PATH_TO_SPLIT_FILES = '../data/splits'
 PATH_TO_IMAGES = '../data/datasets'
@@ -51,6 +52,8 @@ def preprocess_image(image: np.ndarray) -> torch.Tensor:
 def identify_acpds() -> None:
     model_path = "../data/models/80_20_split/acpds/all/5_epochs/mobilenet.pth"
 
+    time_detections = []
+
     model_state_dict = torch.load(model_path, map_location=device)
 
     model = test_model.load_model(model_path)
@@ -82,6 +85,7 @@ def identify_acpds() -> None:
         x3, y3 = coordinates[2]
         x4, y4 = coordinates[3]
 
+        start_time = time.time()
         transformed_image = perspective_transform(parking_lot, coordinates)
 
         transformed_image = preprocess_image(transformed_image)
@@ -89,6 +93,8 @@ def identify_acpds() -> None:
         prediction = model(transformed_image.unsqueeze(0).to(device))
 
         prediction = torch.argmax(prediction, dim=1).item()
+        end_time = time.time()
+        time_detections.append(end_time - start_time)
 
         if prediction == 0:
             color = (0, 255, 0)
@@ -99,6 +105,9 @@ def identify_acpds() -> None:
         cv2.line(parking_lot, (x2, y2), (x3, y3), color, 3)
         cv2.line(parking_lot, (x3, y3), (x4, y4), color, 3)
         cv2.line(parking_lot, (x4, y4), (x1, y1), color, 3)
+
+    print(f"Average time for detection: {sum(time_detections) / len(all_locations)}, "
+          f"Total time: {sum(time_detections)}")
 
     parking_lot = cv2.resize(parking_lot, (1000, 800))
     cv2.imshow('image', parking_lot)
@@ -151,6 +160,8 @@ def identify_cnr_park_ext() -> None:
     model.load_state_dict(model_state_dict)
     model.eval()
 
+    total_time = 0
+
     # each row is represented as [id, x1, y1, width, height]
     for row in rows:
         x1 = int(row[1])
@@ -158,6 +169,7 @@ def identify_cnr_park_ext() -> None:
         width = int(row[3])
         height = int(row[4])
 
+        start_time = time.time()
         parking_spot = whole_parking_lot[y1:y1 + height, x1:x1 + width]
 
         parking_spot = cv2.resize(parking_spot, (IMAGE_WIDTH, IMAGE_HEIGHT))
@@ -167,6 +179,7 @@ def identify_cnr_park_ext() -> None:
         prediction = model(parking_spot.unsqueeze(0).to(device))
 
         prediction = torch.argmax(prediction, dim=1).item()
+        end_time = time.time()
 
         if prediction == 0:
             color = (0, 255, 0)
@@ -174,6 +187,8 @@ def identify_cnr_park_ext() -> None:
             color = (0, 0, 255)
 
         cv2.rectangle(whole_parking_lot, (x1, y1), (x1 + width, y1 + height), color, 3)
+
+    print(f"Average time for detection: {total_time / len(rows)}, Total time: {total_time}")
 
     whole_parking_lot = cv2.resize(whole_parking_lot, (1000, 800))
     cv2.imshow('image', whole_parking_lot)
@@ -186,9 +201,168 @@ def identify_cnr_park_ext() -> None:
     cv2.destroyAllWindows()
 
 
+def identify_spkl() -> None:
+    model_path = "../data/models/80_20_split/spkl/all/5_epochs/mobilenet.pth"
+
+    time_detections = []
+
+    model_state_dict = torch.load(model_path, map_location=device)
+
+    model = test_model.load_model(model_path)
+    model.load_state_dict(model_state_dict)
+    model.eval()
+
+    full_images_path = os.path.join(PATH_TO_FULL_IMAGES, 'spkl', 'images')
+    annotations_path = os.path.join(PATH_TO_FULL_IMAGES, 'spkl', 'int_markup')
+
+    random_int = random.randint(0, len(os.listdir(full_images_path)) - 1)
+    one_image = os.listdir(full_images_path)[random_int]
+    one_annotation = os.listdir(annotations_path)[random_int]
+
+    all_locations = []
+
+    with open(os.path.join(annotations_path, one_annotation)) as f:
+        json_annotation = json.load(f)
+
+    for coordinate in json_annotation['lots']:
+        all_locations.append((coordinate['coordinates'], coordinate['label']))
+
+    parking_lot = cv2.imread(os.path.join(full_images_path, one_image))
+
+    for location in all_locations:
+        coordinates = location[0]
+        label = location[1]
+        x1, y1 = coordinates[0]
+        x2, y2 = coordinates[1]
+        x3, y3 = coordinates[2]
+        x4, y4 = coordinates[3]
+
+        start_time = time.time()
+        transformed_image = perspective_transform(parking_lot, coordinates)
+
+        transformed_image = preprocess_image(transformed_image)
+
+        prediction = model(transformed_image.unsqueeze(0).to(device))
+
+        prediction = torch.argmax(prediction, dim=1).item()
+        end_time = time.time()
+        time_detections.append(end_time - start_time)
+
+        if prediction == 0:
+            color = (0, 255, 0)
+        else:
+            color = (0, 0, 255)
+
+        cv2.line(parking_lot, (x1, y1), (x2, y2), color, 3)
+        cv2.line(parking_lot, (x2, y2), (x3, y3), color, 3)
+        cv2.line(parking_lot, (x3, y3), (x4, y4), color, 3)
+        cv2.line(parking_lot, (x4, y4), (x1, y1), color, 3)
+
+    print(f"Average time for detection: {sum(time_detections) / len(all_locations)}, "
+          f"Total time: {sum(time_detections)}")
+
+    parking_lot = cv2.resize(parking_lot, (1000, 800))
+    cv2.imshow('image', parking_lot)
+    cv2.waitKey(0)
+
+    if not os.path.exists(os.path.join(SAVE_DIR, 'spkl')):
+        os.makedirs(os.path.join(SAVE_DIR, 'spkl'))
+
+    cv2.imwrite(os.path.join(SAVE_DIR, 'spkl', one_image), parking_lot)
+    cv2.destroyAllWindows()
+
+
+def element_to_dict(element):
+    elem_dict = {'tag': element.tag, 'attributes': element.attrib, 'children': []}
+    for child in element:
+        elem_dict['children'].append(element_to_dict(child))
+    return elem_dict
+
+
+def identify_pklot() -> None:
+    model_path = "../data/models/80_20_split/pklot/all/5_epochs/mobilenet.pth"
+
+    time_detections = []
+
+    model_state_dict = torch.load(model_path, map_location=device)
+
+    model = test_model.load_model(model_path)
+    model.load_state_dict(model_state_dict)
+    model.eval()
+
+    all_images = []
+
+    # print all files in the directory
+    full_images_path = os.path.join(PATH_TO_FULL_IMAGES, 'pklot')
+    # print every file
+    for root, dirs, files in os.walk(full_images_path):
+        for file in files:
+            if file.endswith('.jpg'):
+                one_image = os.path.join(root, file)
+                all_images.append(one_image)
+                break
+
+    random_int = random.randint(0, len(all_images) - 1)
+    one_image = all_images[random_int]
+    one_annotation = one_image.replace('.jpg', '.xml')
+
+    tree = ET.parse(one_annotation)
+    root = tree.getroot()
+
+    spaces = []
+    for space in root.findall('space'):
+        space_dict = element_to_dict(space)
+        spaces.append(space_dict)
+
+    parking_lot = cv2.imread(one_image)
+
+    for space in spaces:
+        contour = space['children'][1]
+
+        points = []
+        for point in contour['children']:
+            x = int(point['attributes']['x'])
+            y = int(point['attributes']['y'])
+            points.append((x, y))
+
+        start_time = time.time()
+        transformed_image = perspective_transform(parking_lot, points)
+
+        transformed_image = preprocess_image(transformed_image)
+
+        prediction = model(transformed_image.unsqueeze(0).to(device))
+
+        prediction = torch.argmax(prediction, dim=1).item()
+        end_time = time.time()
+        time_detections.append(end_time - start_time)
+
+        if prediction == 0:
+            color = (0, 255, 0)
+        else:
+            color = (0, 0, 255)
+
+        for i in range(len(points)):
+            cv2.line(parking_lot, points[i], points[(i + 1) % len(points)], color, 3)
+
+    print(f"Average time for detection: {sum(time_detections) / len(spaces)}, "
+          f"Total time: {sum(time_detections)}")
+
+    parking_lot = cv2.resize(parking_lot, (1000, 800))
+    cv2.imshow('image', parking_lot)
+    cv2.waitKey(0)
+
+    if not os.path.exists(os.path.join(SAVE_DIR, 'pklot')):
+        os.makedirs(os.path.join(SAVE_DIR, 'pklot'))
+
+    cv2.imwrite(os.path.join(SAVE_DIR, 'pklot', one_image.split('/')[-1]), parking_lot)
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
     sys.path.append('../')
     import model_visualization.test_models as test_model
 
-    identify_acpds()
+    # identify_acpds()
     # identify_cnr_park_ext()
+    # identify_spkl()
+    # identify_pklot()
